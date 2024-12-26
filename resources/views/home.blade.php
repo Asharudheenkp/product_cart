c340669de56daf084b8056877f57211012c796a9
Asharudheenkp <asharudheenkpi786@gmail.com>
2024-12-26 11:06:57 +0530

    user auth

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Products</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body>
+        <div class="w-[80%] mx-auto">
+            <h1 class="text-4xl mt-28 text-center">Products</h1>
+
+            <div>
+                <img src="" alt="">
+            </div>
+        </div>
+    </body>
+</html>
